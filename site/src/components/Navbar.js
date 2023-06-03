a9aa81a38ee8c10feb26cb20c5d38a561c0683c7
Douglas <125305607+DouglasWillianBasilio@users.noreply.github.com>
2023-06-03 14:03:54 -0300

    colocando atributos na home e css

--- site/src/components/Navbar.js
+++ site/src/components/Navbar.js
@@ -1,20 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import logo from "../assets/pizzaLogo.png"
 import { Link } from 'react-router-dom';
+import ReorderIcon from '@mui/icons-material/Reorder';
 import style from '../styles/Navbar.css'
 
 
 function Navbar() {
+
+    const [openLinks, setOpenLinks] = useState(false)
+    const toggleNavbar = () => {
+        setOpenLinks(!openLinks);
+    };
+
     return (
         <div className='navbar'>
-            <div className='leftSide'>
+            <div className='leftSide' id={openLinks ? "open" : "close"}>
                 <img src={logo} />
+                <div className='hiddenLinks'>
+                    <Link to="/"> Home </Link>
+                    <Link to="/menu"> Menu </Link>
+                    <Link to="/sobre"> Sobre </Link>
+                    <Link to="/contato"> Contato </Link>
+                </div>
             </div>
             <div className='rightSide'>
                 <Link to="/"> Home </Link>
                 <Link to="/menu"> Menu </Link>
                 <Link to="/sobre"> Sobre </Link>
                 <Link to="/contato"> Contato </Link>
+                <button onClick={toggleNavbar}>
+                    <ReorderIcon />
+                </button>
             </div>
         </div>
     )
--- site/src/pages/Home.js
+++ site/src/pages/Home.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import styles from '../styles/Home.css';
+import { Link } from "react-router-dom";
+import BannerImage from "../assets/pizza.jpeg"
 
 function Home() {
-  return (
-    <div>
-      <h1>HOME</h1>
-    </div>
-  )
+    return (
+        <div className='home'>
+            <div className='headerContainer' 
+            style={{ backgroundImage: `url(${BannerImage})` }}>
+                <h1> Dougla'S Pizzaria </h1>
+                <p> PIZZA PARA TODOS OS GOSTOS</p>
+                <Link to="/menu">
+                    <button> PEÇA AGORA </button>
+                </Link>
+            </div>
+        </div>
+    )
 }
 
 export default Home
